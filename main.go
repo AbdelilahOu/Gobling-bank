0fb041e1f53cb7fc8323316c48b49b68a78c0d93
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-14 14:54:04 +0000

    use chi for routing

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main(){
+	// chi router
+	router := chi.NewRouter()
+	// use middleware
+	router.Use(middleware.Logger)
+	// get hello world
+	router.Get("/hello-world",basicHandler)
+	// create server
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
-
+	// handle errors
 	err := server.ListenAndServe()
-	
 	if err != nil {
 		fmt.Println("error starting server : ", err)
 	}
