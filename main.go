d7598028b91b145be7e4933c5e3e26af09267846
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-14 14:42:55 +0000

    basic server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main(){
-	fmt.Println("hello world")
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	
+	if err != nil {
+		fmt.Println("error starting server : ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello World"))
 }
