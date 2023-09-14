2f474e69c036c83571001fb6621e12449c343b7b
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-14 21:57:10 +0000

    working on graceful shotdown

--- application/app.go
+++ application/app.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"context"
 	"net/http"
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{})
 	}
 
 	return app
@@ -25,10 +28,15 @@ func (a *App) Start(ctx context.Context) error {
 		Addr: ":3000",
 		Handler: a.router,
 	}
-	// handle errors
-	err := server.ListenAndServe()
+	// redis health check
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		fmt.Println("error starting server : %w", err)
+		return fmt.Errorf("error connecting to redis : %w", err)
+	}
+	// handle SERVER errors
+	err = server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("error starting server : %w", err)
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"context"
+	"os/signal"
 	"github.com/AbdelilahOu/GoThingy/application"
 )
 
 func main(){
+	// create app instance
 	app := application.New()
-
-	err := app.Start(context.TODO())
-
+	// use signals
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	// start app
+	err := app.Start(ctx)
+	// app errors
 	if err != nil {
 		fmt.Println("failed to start app : %w",err)
 	}
