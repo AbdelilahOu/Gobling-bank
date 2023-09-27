9664f0099ef93bc1b2d49a0b0fb46afaae73559b
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-27 10:08:57 +0000

    adding postgres

--- application/app.go
+++ application/app.go
@@ -2,21 +2,39 @@ package application
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
 	rdb    *redis.Client
+	psql   *sql.DB
 }
 
 func New() *App {
+	err := godotenv.Load()
+
+	if err != nil {
+		println(".env doesnt exist!", err)
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		println("error connecting to database", err)
+	}
+	// driver, err := postgres.WithInstance(db, &postgres.Config{})
+
 	app := &App{
 		router: loadRoutes(),
 		rdb:    redis.NewClient(&redis.Options{}),
+		psql:   db,
 	}
 
 	return app
