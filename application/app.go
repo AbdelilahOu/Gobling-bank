db62d6f069089768492d2aa2af887e95d809acda
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-27 18:13:58 +0000

    clean up

--- application/app.go
+++ application/app.go
@@ -2,19 +2,14 @@ package application
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/sqlite"
 	"github.com/joho/godotenv"
 )
 
 type App struct {
 	router http.Handler
-	db     *sql.DB
 }
 
 func New() *App {
@@ -24,18 +19,9 @@ func New() *App {
 	if err != nil {
 		println(".env doesnt exist!", err)
 	}
-	// log db string
-	println("connecting to db", os.Getenv("DATABASE_URL"))
-	// created db connection
-	db, err := sql.Open("sqlite", "sqlite://db/db.sqlite")
-	// check if theres errro
-	if err != nil {
-		println("error connecting to database", err.Error())
-	}
 	// create app instance
 	app := &App{
 		router: loadRoutes(),
-		db:     db,
 	}
 
 	return app
@@ -47,31 +33,8 @@ func (a *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	// health check the db connection
-	res, err := a.db.Query("SELECT 1", 1)
-	println(res)
-	if err != nil {
-		println("error pinging db", err)
-	}
-	// created driver & check for error
-	driver, err := sqlite.WithInstance(a.db, &sqlite.Config{})
-	if err != nil {
-		println("error creating driver", err)
-	}
-	// create migration instance & check if theres error
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://db/migrations",
-		"sqlite", driver,
-	)
-	if err != nil {
-		println("error creating migration instance")
-	}
-	// run migration
-	if err := m.Up(); err != nil {
-		println("error running migration")
-	}
 	// handle SERVER errors
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("error starting server : %w", err)
 	}
--- go.mod
+++ go.mod
@@ -3,31 +3,6 @@ module github.com/AbdelilahOu/GoThingy
 go 1.20
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
-	github.com/golang-migrate/migrate/v4 v4.16.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/redis/go-redis/v9 v9.1.0 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.1 // indirect
-	lukechampine.com/uint128 v1.2.0 // indirect
-	modernc.org/cc/v3 v3.36.3 // indirect
-	modernc.org/ccgo/v3 v3.16.9 // indirect
-	modernc.org/libc v1.17.1 // indirect
-	modernc.org/mathutil v1.5.0 // indirect
-	modernc.org/memory v1.2.1 // indirect
-	modernc.org/opt v0.1.3 // indirect
-	modernc.org/sqlite v1.18.1 // indirect
-	modernc.org/strutil v1.1.3 // indirect
-	modernc.org/token v1.0.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/joho/godotenv v1.5.1
 )
