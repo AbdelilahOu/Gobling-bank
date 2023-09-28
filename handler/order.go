aba7d933e7cfb8cf8833fdca3806177458b7443b
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-28 09:12:46 +0000

    handlers

--- handler/order.go
+++ handler/order.go
@@ -3,28 +3,30 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/AbdelilahOu/GoThingy/repository"
 )
 
-type Order struct{}
+type Order struct {
+	Repo *repository.OrderRepo
+}
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request)  {
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("create order")
 }
 
-func (o *Order) GetAll(w http.ResponseWriter, r *http.Request)  {
+func (o *Order) GetAll(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get orders")
 }
 
-func (o *Order) GetByID(w http.ResponseWriter, r *http.Request)  {
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get order")
 }
 
-func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request)  {
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("update order")
 }
 
-func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request)  {
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("delete order")
 }
-
-
