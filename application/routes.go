fc26b8043766d60542a337279e75730622e85b0a
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-28 09:13:24 +0000

    change

--- application/routes.go
+++ application/routes.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/AbdelilahOu/GoThingy/handler"
+	"github.com/AbdelilahOu/GoThingy/repository"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() *chi.Mux {
 	// create chi router
 	router := chi.NewRouter()
 	//  use logger middleware
@@ -18,14 +19,18 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 	// sub routes
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 	//
 	return router
 }
 
-func loadOrderRoutes(router chi.Router) {
+func (a *App) loadOrderRoutes(router chi.Router) {
 	// get order handler
-	orderHandler := &handler.Order{}
+	orderHandler := &handler.Order{
+		Repo: &repository.OrderRepo{
+			DB: a.db,
+		},
+	}
 	// attach routes
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.GetAll)
