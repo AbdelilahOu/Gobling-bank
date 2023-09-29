010fee9c57fcbeac9b871934a4a66d0380f93fd7
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-29 09:30:28 +0000

    create cleint repo

---
+++ repository/clientRepo.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/AbdelilahOu/GoThingy/model"
+)
+
+type ClientRepo struct {
+	DB *sql.DB
+}
+
+func (repo *ClientRepo) Insert(ctx context.Context, client model.Client) error {
+	repo.DB.Query("INSERT INTO Clients")
+
+	return nil
+}
+
+func (repo *ClientRepo) Update(ctx context.Context, client model.Client, id string) error {
+	return nil
+}
+
+func (repo *ClientRepo) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+func (repo *ClientRepo) Select(ctx context.Context, id string) error {
+	return nil
+}
