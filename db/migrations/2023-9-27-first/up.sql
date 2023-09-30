7356ee38f25910f09741b6d5e19c04bebbaf49f0
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-30 10:32:16 +0000

    change schema

--- db/migrations/2023-9-27-first/up.sql
+++ db/migrations/2023-9-27-first/up.sql
@@ -1,17 +1,8 @@
--- Create the "users" table
-CREATE TABLE IF NOT EXISTS users (
-  id TEXT NOT NULL PRIMARY KEY,
-  username TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  password TEXT NOT NULL,
-  email TEXT NOT NULL,
-  role TEXT NOT NULL DEFAULT 'user'
-);
-
 -- Create the "clients" table
 CREATE TABLE IF NOT EXISTS clients (
   id TEXT NOT NULL PRIMARY KEY,
-  fullname TEXT NOT NULL,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL,
   phone TEXT NOT NULL DEFAULT '',
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   email TEXT NOT NULL DEFAULT '',
@@ -28,38 +19,6 @@ CREATE TABLE IF NOT EXISTS products (
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   price REAL NOT NULL,
   tva REAL NOT NULL DEFAULT 0
-);
-
--- Create the "sellers" table
-CREATE TABLE IF NOT EXISTS sellers (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  phone TEXT NOT NULL DEFAULT '',
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  email TEXT NOT NULL DEFAULT '',
-  address TEXT NOT NULL DEFAULT '',
-  image TEXT NOT NULL DEFAULT ''
-);
-
--- Create the "invoices" table
-CREATE TABLE IF NOT EXISTS invoices (
-  id TEXT NOT NULL PRIMARY KEY,
-  status TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  client_id TEXT NOT NULL,
-  CONSTRAINT invoices_client_id_fkey FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- Create the "invoice_items" table
-CREATE TABLE IF NOT EXISTS invoice_items (
-  id TEXT NOT NULL PRIMARY KEY,
-  product_id TEXT NOT NULL,
-  invoice_id TEXT NOT NULL,
-  quantity BIGINT NOT NULL,
-  inventory_id TEXT NOT NULL,
-  CONSTRAINT invoice_items_product_id_fkey FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT invoice_items_invoice_id_fkey FOREIGN KEY (invoice_id) REFERENCES invoices (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT invoice_items_inventory_id_fkey FOREIGN KEY (inventory_id) REFERENCES inventory_mouvements (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create the "orders" table
@@ -98,7 +57,5 @@ CREATE TABLE IF NOT EXISTS inventory_mouvements (
 -- Create unique indexes
 CREATE UNIQUE INDEX IF NOT EXISTS order_items_inventory_id_key ON order_items (inventory_id);
 CREATE UNIQUE INDEX IF NOT EXISTS order_items_id_key ON order_items (id);
-CREATE UNIQUE INDEX IF NOT EXISTS invoice_items_inventory_id_key ON invoice_items (inventory_id);
 CREATE UNIQUE INDEX IF NOT EXISTS products_name_key ON products (name);
-CREATE UNIQUE INDEX IF NOT EXISTS clients_fullname_key ON clients (fullname);
-CREATE UNIQUE INDEX IF NOT EXISTS sellers_name_key ON sellers (name);
+CREATE UNIQUE INDEX IF NOT EXISTS clients_email_key ON clients (email);
