05705e1baf765f1ca0809935f146e6242bc94a9a
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-30 11:55:03 +0000

    refactor

--- application/app.go
+++ application/app.go
@@ -5,7 +5,11 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"path/filepath"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -24,45 +28,80 @@ const (
 )
 
 func New() *App {
-	// load env variables
+	// Load environment variables
 	err := godotenv.Load()
-	// check if .env exists
 	if err != nil {
-		println(".env doesnt exist!", err)
+		fmt.Println(".env doesn't exist or couldn't be loaded:", err)
 	}
-	// connection string
+
+	// Connection string
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	// connect
+
+	// Connect to the database
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		println("error connecting to db")
+		fmt.Println("Error connecting to the database:", err)
 	}
+	// defer db.Close()
+
+	// Ping the database
 	err = db.Ping()
 	if err != nil {
-		println("error pinging db")
+		fmt.Println("Error pinging the database:", err)
 	}
-	defer db.Close()
-	// create app instance
+
 	app := &App{
 		db: db,
 	}
-	// load routes
+
+	// Load routes
 	app.loadRoutes()
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
-	// create server
+	// Create a server
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	// handle SERVER errors
-	err := server.ListenAndServe()
+
+	// Run migrations
+	driver, err := postgres.WithInstance(a.db, &postgres.Config{})
 	if err != nil {
-		return fmt.Errorf("error starting server : %w", err)
+		fmt.Println("Error getting driver:", err)
+		return err
+	}
+	// migrations path
+	migrationsPath, err := filepath.Rel(".", "./db/migrations")
+	if err != nil {
+		fmt.Println("Error getting absolute path:", err)
+		return err
+	}
+	// Specify the correct path to your migrations directory
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+migrationsPath,
+		"postgres", driver,
+	)
+	if err != nil {
+		fmt.Println("Error creating migrations instance:", err)
+		return err
+	}
+
+	// Apply migrations
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		fmt.Println("Error applying migrations:", err)
+		return err
+	}
+
+	// Handle server errors
+	err = server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error starting the server: %v\n", err)
+		return err
 	}
 
 	return nil
