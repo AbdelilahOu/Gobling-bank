32d13afc065c5056661d116ff52ede31924350e1
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-05 22:28:48 +0000

    migrations are working nice

---
+++ db/000001_first.down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS clients;
+-- DROP TABLE IF EXISTS orders;
+-- DROP TABLE IF EXISTS order_items;
+-- DROP TABLE IF EXISTS products;
+-- DROP TABLE IF EXISTS inventory_mouvements;
+
+-- COMMIT;
--- db/migrations/2023_9_27_first.down.sql
+++
@@ -1,6 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS order_items;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS inventory_mouvements;
