4c48c22272ef4f534067b4e37e78abc21195c7c3
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-07 10:17:58 +0000

    create product done

--- handler/product.go
+++ handler/product.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/AbdelilahOu/GoThingy/model"
 	"github.com/AbdelilahOu/GoThingy/repository"
+	"github.com/google/uuid"
 )
 
 type Product struct {
@@ -11,6 +16,54 @@ type Product struct {
 }
 
 func (o *Product) Create(w http.ResponseWriter, r *http.Request) {
+	// bosy struct
+	var body struct {
+		Name        string  `json:"name"`
+		Price       float64 `json:"price"`
+		Description string  `json:"description"`
+		Tva         float64 `json:"tva"`
+	}
+	// get body
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// timr
+	now := time.Now().UTC()
+	// uuid
+	id, err := uuid.NewUUID()
+	if err != nil {
+		fmt.Println("error generating uuid")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// create product
+	product := model.Product{
+		Id:          id,
+		Name:        body.Name,
+		Price:       body.Price,
+		Description: body.Description,
+		Tva:         body.Tva,
+		Created_at:  &now,
+	}
+	// add to db
+	err = o.Repo.Insert(r.Context(), product)
+	if err != nil {
+		fmt.Println("error inserting product")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//
+	res, err := json.Marshal(product)
+	if err != nil {
+		fmt.Println("error marshaling product")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
+
 }
 
 func (o *Product) GetAll(w http.ResponseWriter, r *http.Request) {
