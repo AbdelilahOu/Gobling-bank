fd723a1968da9fc8ec90bdda8d10707618d810a2
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-08 09:28:18 +0000

    order handler : create

--- handler/order.go
+++ handler/order.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/AbdelilahOu/GoThingy/repository"
@@ -11,6 +13,24 @@ type Order struct {
 }
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	// body struct
+	var body struct {
+		ClientId string `json:"client_id"`
+		Status   string `json:"status"`
+		Items    []struct {
+			ProductId string  `json:"product_id"`
+			Quantity  int     `json:"quantity"`
+			NewPrice  float64 `json:"new_price"`
+		} `json:"items"`
+	}
+	// decode body
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println(body.ClientId)
 }
 
 func (o *Order) GetAll(w http.ResponseWriter, r *http.Request) {
