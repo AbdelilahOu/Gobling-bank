b6e1103a2a9080f70a6152dda7ceeeb46d195562
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-11 11:52:48 +0000

    move queries to vars

--- repository/orderRepo.go
+++ repository/orderRepo.go
@@ -20,7 +20,8 @@ type GetOAllResult struct {
 }
 
 func (repo *OrderRepo) Insert(ctx context.Context, order model.Order) error {
-	_, err := repo.DB.Exec("INSERT INTO orders (id, client_id, status) VALUES ($1, $2, $3)", order.Id, order.ClientId, order.Status)
+	InsertQuery := "INSERT INTO orders (id, client_id, status) VALUES ($1, $2, $3)"
+	_, err := repo.DB.Exec(InsertQuery, order.Id, order.ClientId, order.Status)
 	if err != nil {
 		fmt.Println("error inserting order", err)
 		return err
@@ -29,7 +30,8 @@ func (repo *OrderRepo) Insert(ctx context.Context, order model.Order) error {
 }
 
 func (repo *OrderRepo) Update(ctx context.Context, order model.Order, id string) error {
-	_, err := repo.DB.Exec("UPDATE orders SET status = $1 WHERE id = $2", order.Status, id)
+	UpdateQuery := "UPDATE orders SET status = $1 WHERE id = $2"
+	_, err := repo.DB.Exec(UpdateQuery, order.Status, id)
 
 	if err != nil {
 		fmt.Println("error updating order :", err)
@@ -39,7 +41,8 @@ func (repo *OrderRepo) Update(ctx context.Context, order model.Order, id string)
 }
 
 func (repo *OrderRepo) Delete(ctx context.Context, id string) error {
-	_, err := repo.DB.Exec("DELETE FROM orders WHERE id = $1", id)
+	DeleteQuery := "DELETE FROM orders WHERE id = $1"
+	_, err := repo.DB.Exec(DeleteQuery, id)
 	if err != nil {
 		fmt.Println("error deleting order", err)
 		return err
@@ -49,8 +52,9 @@ func (repo *OrderRepo) Delete(ctx context.Context, id string) error {
 
 func (repo *OrderRepo) Select(ctx context.Context, id string) (model.Order, error) {
 	// execute
+	SelectQuery := "SELECT * FROM orders WHERE id = $1"
 	var order model.Order
-	err := repo.DB.Select(&order, "SELECT * FROM orders WHERE id = $1", id)
+	err := repo.DB.Select(&order, SelectQuery, id)
 	// var
 	if err == sql.ErrNoRows {
 		fmt.Println("no redcord exisist", err)
@@ -68,8 +72,9 @@ func (repo *OrderRepo) Select(ctx context.Context, id string) (model.Order, erro
 
 func (repo *OrderRepo) SelectAll(ctx context.Context, cursor uint64, size uint64) (GetOAllResult, error) {
 	// get orders
+	SelectAllQuery := "SELECT id, client_id, status, created_at  FROM orders WHERE id > $1 LIMIT $2"
 	var orders []model.Order
-	err := repo.DB.Select(&orders, "SELECT id, client_id, status, created_at  FROM orders WHERE id > $1 LIMIT $2", cursor, size)
+	err := repo.DB.Select(&orders, SelectAllQuery, cursor, size)
 	if err != nil {
 		fmt.Println("error getting orders", err)
 		return GetOAllResult{}, err
