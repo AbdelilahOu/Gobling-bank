8b7b05775319aa1a99eb7689d67258635f42d4a3
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-21 11:32:13 +0000

    add utils for cors

---
+++ utils/cors.go
@@ -0,0 +1,8 @@
+package utils
+
+import "net/http"
+
+func EnableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Content-Type", "application/json")
+}
