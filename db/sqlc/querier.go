a4bb49d5cf9170d954dd91c0e8c1cc46fc0d0f5d
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-03 14:45:11 +0000

    generate go functions

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -9,7 +9,11 @@ import (
 )
 
 type Querier interface {
+	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	DeleteAccount(ctx context.Context, id int64) error
 	GetAcount(ctx context.Context, id int64) (Account, error)
+	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
+	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
 }
 
 var _ Querier = (*Queries)(nil)
