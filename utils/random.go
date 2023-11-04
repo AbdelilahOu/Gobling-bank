7f001e2337341914745dcf998e42d081de0a33c3
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-04 15:24:56 +0000

    delete dupricated func

--- utils/random.go
+++ utils/random.go
@@ -2,12 +2,11 @@ package utils
 
 import (
 	"math/rand"
-	"time"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+// func init() {
+// 	rand.Seed(time.Now().UnixNano())
+// }
 
 func RandomInt(min, max int64) int64 {
 	return rand.Int63n(max-min+1) + min
