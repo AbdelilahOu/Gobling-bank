474b25d6e337243b1aa5d16013130f7c64a2fdb6
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-04 13:09:16 +0000

    add config

--- config/dbConfig.go
+++ config/dbConfig.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"os"
+)
+
+type DbConfig struct {
+	User     string
+	Password string
+	Host     string
+	Post     int
+}
+type Config struct {
+	Db DbConfig
+}
+
+func LoadConfig() *Config {
+	var dbCfg DbConfig
+
+	if password, exists := os.LookupEnv("POSTGRES_PASSWORD"); exists {
+		dbCfg.Password = password
+	} else {
+		dbCfg.Password = "mysecretpassword"
+	}
+
+	if user, exists := os.LookupEnv("POSTGRES_USER"); exists {
+		dbCfg.User = user
+	} else {
+		dbCfg.User = "root"
+	}
+
+	if host, exists := os.LookupEnv("POSTGRES_HOST"); exists {
+		dbCfg.Host = host
+	} else {
+		dbCfg.User = "localhost"
+	}
+
+	dbCfg.Post = 5432
+
+	return &Config{
+		Db: dbCfg,
+	}
+}
