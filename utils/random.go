e13c2198b4607f58cb535826d45a01efbdca6889
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-04 12:44:16 +0000

    add utils

---
+++ utils/random.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return rand.Int63n(max-min+1) + min
+}
+
+func RandomString(length int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	s := make([]rune, length)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomAmount() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
