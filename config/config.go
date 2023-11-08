eb7d16e16bfbdf5104adfce82a1fa34533972fc9
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-08 14:42:42 +0000

    add laod config

--- config/config.go
+++ config/config.go
@@ -1,11 +1,24 @@
 package config
 
+import "github.com/spf13/viper"
+
 type Config struct {
-	DB DbConfig
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_Source"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
 }
 
-func NewConfig() *Config {
-	return &Config{
-		DB: LoadDbConfig(),
+func LoadConfig(path string) (c Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
 	}
+
+	err = viper.Unmarshal(&c)
+	return
 }
