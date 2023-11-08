e4dd1131a02b657e626c8faa89a84de30e47f56e
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-08 14:29:07 +0000

    create server

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "github.com/AbdelilahOu/GoThingy/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// server serves http requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// create new server
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+	router.PUT("/accounts/:id", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	server.router = router
+	return server
+}
+
+// start server
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
