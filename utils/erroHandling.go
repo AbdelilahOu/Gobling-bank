d63efb614bd12586a80dbe87c4618bf8287b53ef
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-10 15:12:48 +0000

    add currency error checking

--- utils/erroHandling.go
+++ utils/erroHandling.go
@@ -1,7 +1,16 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func ErrorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func ErrInvalidCurrency(accountID uuid.UUID, AccCurrency, currency string) error {
+	return fmt.Errorf("account %d currency mismatch %s vs %s", accountID, AccCurrency, currency)
 }
