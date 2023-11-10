82c314f59b236dca36de2452b6e7b28db214786e
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-10 15:27:26 +0000

    update randomCurrency

--- utils/random.go
+++ utils/random.go
@@ -30,7 +30,7 @@ func RandomAmount() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
