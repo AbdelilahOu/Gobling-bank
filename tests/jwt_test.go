73e6ffd43d5e37d4cffe5b457658e74f1680d91c
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-19 14:31:32 +0000

    jwt test

--- tests/jwt_test.go
+++ tests/jwt_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/AbdelilahOu/GoThingy/token"
 	"github.com/AbdelilahOu/GoThingy/utils"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,4 +32,38 @@ func TestJWTMaker(t *testing.T) {
 	require.Equal(t, username, payload.Username)
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJWT(t *testing.T) {
+	maker, err := token.NewJWTMaker(utils.RandomString(32))
+	require.NoError(t, err)
+
+	username := utils.RandomOwner()
+	duration := -time.Minute
+
+	createdToken, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, createdToken)
+
+	payload, err := maker.VerifyToken(createdToken)
+	require.Error(t, err)
+	require.EqualError(t, err, token.ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlg(t *testing.T) {
+	maker, err := token.NewJWTMaker(utils.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err := token.NewPayload(utils.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	signedToken, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(signedToken)
+	require.Error(t, err)
+	require.EqualError(t, err, token.ErrInvalidToken.Error())
+	require.Nil(t, payload)
 }
