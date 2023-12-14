891eb33b4d2079dcb67afe22294847148abc22b0
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-14 08:46:41 +0000

    FEAT: protect transfer handlers

--- api/transfer.go
+++ api/transfer.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/AbdelilahOu/GoThingy/db/sqlc"
+	"github.com/AbdelilahOu/GoThingy/token"
 	"github.com/AbdelilahOu/GoThingy/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -24,11 +26,20 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, utils.ErrorResponse(err))
 		return
 	}
-	if !server.validAccountCurrency(ctx, req.FromAccountID, req.Currency) {
+	fromAccount, ok := server.validAccount(ctx, req.FromAccountID, req.Currency)
+	if !ok {
 		return
 	}
-
-	if !server.validAccountCurrency(ctx, req.ToAccountID, req.Currency) {
+	// get user from token
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	// check if user is the owner of the account
+	if authPayload.Username != fromAccount.Owner {
+		err := errors.New("from account doesn't belong to authenticated user")
+		ctx.JSON(http.StatusUnauthorized, utils.ErrorResponse(err))
+		return
+	}
+	_, ok = server.validAccount(ctx, req.ToAccountID, req.Currency)
+	if !ok {
 		return
 	}
 	// run transaction
@@ -52,22 +63,22 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, result)
 }
 
-func (server *Server) validAccountCurrency(ctx *gin.Context, accountID uuid.UUID, currency string) bool {
+func (server *Server) validAccount(ctx *gin.Context, accountID uuid.UUID, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, utils.ErrorResponse(err))
-			return false
+			return account, false
 		}
 		ctx.JSON(http.StatusInternalServerError, utils.ErrorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := utils.ErrInvalidCurrency(account.ID, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, utils.ErrorResponse(err))
-		return false
+		return account, false
 	}
 
-	return true
+	return account, true
 }
