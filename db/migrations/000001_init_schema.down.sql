52e8b74ebf541aa61e4e166437107e73172a7b80
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-03-02 12:07:40 +0000

    FEAT: fix migrations

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS entries;
-DROP TABLE IF EXISTS transfers;
-DROP TABLE IF EXISTS acounts;
-
 ALTER TABLE entries DROP CONSTRAINT IF EXISTS entries_account_id_fkey;
 ALTER TABLE transfers DROP CONSTRAINT IF EXISTS transfers_from_account_id_fkey;
 ALTER TABLE transfers DROP CONSTRAINT IF EXISTS transfers_to_account_id_fkey;
@@ -14,3 +10,8 @@ DROP INDEX IF EXISTS transfers_from_to_account_id_idx;
 
 COMMENT ON COLUMN entries.amount IS NULL;
 COMMENT ON COLUMN transfers.amount IS NULL;
+
+DROP TABLE IF EXISTS entries;
+DROP TABLE IF EXISTS transfers;
+DROP TABLE IF EXISTS accounts;
+
--- db/migrations/000002_init_user.down.sql
+++ db/migrations/000002_init_user.down.sql
@@ -3,4 +3,3 @@ ALTER TABLE IF EXISTS accounts DROP CONSTRAINT IF EXISTS "owner_currency_key";
 ALTER TABLE IF EXISTS accounts DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
 
 drop table users;
-
